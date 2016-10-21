dbe0849927f2d5fcb5b19b7a8f7cf7af41f78eb4
Aayush Kapoor <xeoneux@icloud.com>
2016-10-21 16:22:52 +1100

    Save movie data to movies array

--- app/src/main/java/com/xeoneux/popularmovies/MainActivity.java
+++ app/src/main/java/com/xeoneux/popularmovies/MainActivity.java
@@ -6,10 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Movie> movies = new ArrayList<>();
     AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 
     @Override
@@ -29,7 +36,29 @@ public class MainActivity extends AppCompatActivity {
         asyncHttpClient.get(url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    JSONObject response = new JSONObject(new String(responseBody));
+                    JSONArray results = response.getJSONArray("results");
 
+                    movies.clear();
+
+                    for (int i = 0; i < results.length(); i++) {
+                        Movie movie = new Movie();
+
+                        JSONObject result = results.getJSONObject(i);
+
+                        movie.title = result.getString("title");
+                        movie.releaseDate = result.getString("release_date");
+                        movie.moviePoster = result.getString("poster_path");
+                        movie.voteAverage = result.getString("vote_average");
+                        movie.plotSynopsis = result.getString("overview");
+
+                        movies.add(movie);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
---
+++ app/src/main/java/com/xeoneux/popularmovies/Movie.java
@@ -0,0 +1,9 @@
+package com.xeoneux.popularmovies;
+
+public class Movie {
+    public String title;
+    public String releaseDate;
+    public String moviePoster;
+    public String voteAverage;
+    public String plotSynopsis;
+}
