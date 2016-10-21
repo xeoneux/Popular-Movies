b8f6b4f9394d27150f7cb508d62d3590bdbd8ab6
Aayush Kapoor <xeoneux@icloud.com>
2016-10-21 15:38:30 +1100

    Async get request for movies

--- app/src/main/java/com/xeoneux/popularmovies/MainActivity.java
+++ app/src/main/java/com/xeoneux/popularmovies/MainActivity.java
@@ -1,13 +1,41 @@
 package com.xeoneux.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fetchMovies("popular");
+    }
+
+    void fetchMovies(String sortCriteria) {
+        String baseUrl = "https://api.themoviedb.org/3/movie/";
+        String apiKey = BuildConfig.THE_MOVIE_DATABASE_API_KEY;
+
+        String url = baseUrl + sortCriteria + "/?api_key=" + apiKey;
+
+        asyncHttpClient.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
     }
 }
